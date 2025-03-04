cc14f69644c4732cb742513709120c1489b3d8bb
LMBRT0 <lambe.rto.archive@gmail.com>
2025-03-04 12:53:37 +0700

    Тест

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello World!\n");
+
+	return 0;
+}
+
+
+// g++ -E test.cpp -o test.i && g++ -S test.i -o test.s && g++ -c test.s -o test.o && g++ test.o -o test
